df8e98cd0584de1b63eedabc1b958d0f271b602c
WolfCannine <119031760+WolfCannine@users.noreply.github.com>
2023-11-06 13:03:49 +0500

    Update GameObjectExtensions.cs
    Directly return the GetComponent if null then AddComponent

--- Assets/_Project/Scripts/Utils/Extensions/GameObjectExtensions.cs
+++ Assets/_Project/Scripts/Utils/Extensions/GameObjectExtensions.cs
@@ -3,8 +3,7 @@
 namespace Utilities {
     public static class GameObjectExtensions {
         public static T GetOrAdd<T>(this GameObject gameObject) where T : Component {
-            T component = gameObject.GetComponent<T>();
-            return component != null ? component : gameObject.AddComponent<T>();
+            return gameObject.GetComponent<T>() ?? gameObject.AddComponent<T>();
         }
     }
 }
